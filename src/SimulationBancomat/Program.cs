77203ebdf4cb69b304e2d7fb4e506910f769f7a4
Kentin Fankhauser <pm27rlp@eduvaud.ch>
2026-01-14 15:56:01 +0100

    feat(Début de programmation): Commencement de programmation du menu

--- src/SimulationBancomat/Program.cs
+++ src/SimulationBancomat/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace SimulationBancomat
+﻿using System;
+
+namespace SimulationBancomat
 {
     class Program
     {
@@ -9,9 +11,35 @@
         /// <param name="args"></param>
         public static void Main(string[] args)
         {
-            
+            Presentation();
+            Console.ReadLine();
         }
 
+        static void Presentation()
+        {
+            Console.Write("\t#############################################################################\n");
+            Console.Write("\t###              Bienvenue à la Banque national Fankhauser !              ###\n");
+            Console.Write("\t#############################################################################\n");
+        }
+
+        static void VerifyCode()
+        {
+
+        }
         
+        static void GetOutMoney()
+        {
+
+        }
+
+        static void SeeAmount()
+        {
+
+        }
+
+        static void PrintReceipt()
+        {
+
+        }
     }
 }
