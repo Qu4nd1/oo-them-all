0a9a059464e9bc4761786002128672c332a46c5b
Kentin Fankhauser <pm27rlp@eduvaud.ch>
2026-01-14 15:34:26 +0100

    chore(initial): init repo

---
+++ src/SimulationBancomat/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace SimulationBancomat
+{
+    class Program
+    {
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            
+        }
+
+        
+    }
+}
