4ccd528bfa05605463e452beecac38171432e0d9
Kentin Fankhauser <pm27rlp@eduvaud.ch>
2026-01-29 16:15:28 +0100

    fix(solde visual): user can now see the amount on his bank acount and press q to go back to the menu

--- src/SimulationBancomat/Program.cs
+++ src/SimulationBancomat/Program.cs
@@ -520,13 +520,20 @@ namespace SimulationBancomat
         static void SeeAmount(decimal bankMoneyAmount)
         {
             string showAmount = $"Solde: {bankMoneyAmount:c}";
-            int screenheigth = 3;
+            int screenheigth = 5;
             int centerWriting = (PasswordData.PASSWORD_SCREEN_X - 21) + ((62 / 2) - (showAmount.Length / 2));
-            
+            char keyChar;
 
             Presentation();
             DrawScreen(PasswordData.PASSWORD_SCREEN_X - 21, PasswordData.PASSWORD_SCREEN_Y - 1, 62, screenheigth);
             DrawAtString(centerWriting, PasswordData.PASSWORD_SCREEN_Y, showAmount);
+            DrawAtCenterString(PasswordData.PASSWORD_SCREEN_X - 21, ((PasswordData.PASSWORD_SCREEN_Y + 2)), 62, "Appuyer sur 'Q' pour revenir au menu des options");
+
+            ConsoleKeyInfo key = Console.ReadKey(true);  // true = ne pas afficher la touche
+            keyChar = key.KeyChar;
+            if (keyChar.ToString().ToUpper() == "Q")
+                return;
+
         }
     }
 }
